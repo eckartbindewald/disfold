ad419bb3ea96f275ac21c1edc97b836d2c311cdf
Eckart Bindewald <eckart.bindewald@precise-soft.com>
2024-03-24 18:30:38 -0400

    changed overwrite flag to TRUE

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,13 @@
 ## code to prepare `DATASET` dataset goes here
+library(here)
 
-disprot23 = read.csv("DisProt release_2023_12 with_ambiguous_evidences.tsv", sep="\t")
+PROJECT_ROOT = here()
+data_raw_dir = file.path(PROJECT_ROOT, 'data-raw')
+filename = file.path(data_raw_dir, "DisProt release_2023_12 with_ambiguous_evidences.tsv")
+disprot23 = read.csv(filename, sep="\t")
+cat("Read", nrow(disprot23), 'rows of file', filename)
 print(head(disprot23))
-usethis::use_data(disprot23)
+usethis::use_data(disprot23, overwrite=TRUE) # change overwrite flag if needed
 
 # usethis::use_data(DATASET, overwrite = TRUE)
 
